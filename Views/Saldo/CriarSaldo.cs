d5df555d811f7ce475a695fce5294313e4b03d6a
rmoreiradematos <rmoreiradematos@gmail.com>
2023-04-27 23:32:24 -0300

    commit de acerto

--- Views/Saldo/CriarSaldo.cs
+++ Views/Saldo/CriarSaldo.cs
@@ -1,104 +1,92 @@
-using Models;
 using Controllers;
+using Models;
+using System;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Views
 {
-    public class EditarSaldo : Form
+    public class CriarSaldo : Form
     {
-        private readonly Label lblIdProduto;
-        private readonly Label lblIdAlmoxarifado;
-        private readonly Label lblQuantidade;
-        private readonly TextBox txtIdProduto;
-        private readonly TextBox txtIdAlmoxarifado;
-        private readonly TextBox txtQuantidade;
-        private readonly Button btnEdit;
-
         private readonly SaldoController saldoController = new SaldoController();
-        private readonly Models.Saldo saldo;
 
-        public EditarSaldo(Models.Saldo saldo)
+        public Label lblIdProduto;
+        public TextBox txtIdProduto;
+        public Label lblIdAlmoxarifado;
+        public TextBox txtIdAlmoxarifado;
+        public Label lblQuantidade;
+        public TextBox txtQuantidade;
+        public Button btnRegister;
+
+        public CriarSaldo()
         {
-            this.saldo = saldo;
-
-            Text = "Editar Saldo";
-            StartPosition = FormStartPosition.CenterScreen;
-            FormBorderStyle = FormBorderStyle.FixedSingle;
-            MaximizeBox = false;
-            MinimizeBox = false;
-            ShowIcon = false;
-            ShowInTaskbar = false;
-            Size = new System.Drawing.Size(350, 250);
-
-            lblIdProduto = new Label
-            {
-                Text = "Id do produto:",
-                Location = new System.Drawing.Point(10, 40),
-                Size = new System.Drawing.Size(150, 20)
-            };
-
-            txtIdProduto = new TextBox
-            {
-                Text = saldo.IdProduto.ToString(),
-                Location = new System.Drawing.Point(170, 40),
-                Size = new System.Drawing.Size(150, 20)
-            };
-
-            lblIdAlmoxarifado = new Label
-            {
-                Text = "Id do almoxarifado:",
-                Location = new System.Drawing.Point(10, 70),
-                Size = new System.Drawing.Size(150, 20)
-            };
-
-            txtIdAlmoxarifado = new TextBox
-            {
-                Text = saldo.IdAlmoxarifado.ToString(),
-                Location = new System.Drawing.Point(170, 70),
-                Size = new System.Drawing.Size(150, 20)
-            };
-
-            lblQuantidade = new Label
-            {
-                Text = "Quantidade:",
-                Location = new System.Drawing.Point(10, 100),
-                Size = new System.Drawing.Size(150, 20)
-            };
-
-            txtQuantidade = new TextBox
-            {
-                Text = saldo.Quantidade.ToString(),
-                Location = new System.Drawing.Point(170, 100),
-                Size = new System.Drawing.Size(150, 20)
-            };
-
-            btnEdit = new Button
-            {
-                Text = "Alterar",
-                Location = new System.Drawing.Point(170, 150),
-                Size = new System.Drawing.Size(150, 35)
-            };
-            btnEdit.Click += BtnEdit_Click;
-
-            Controls.AddRange(new Control[]
-            {
-                lblIdProduto, txtIdProduto, lblIdAlmoxarifado, txtIdAlmoxarifado,
-                lblQuantidade, txtQuantidade, btnEdit
-            });
+            InitializeComponent();
         }
 
-        private void BtnEdit_Click(object sender, EventArgs e)
+        private void InitializeComponent()
         {
-            saldo.IdProduto = int.Parse(txtIdProduto.Text);
-            saldo.IdAlmoxarifado = int.Parse(txtIdAlmoxarifado.Text);
-            saldo.Quantidade = int.Parse(txtQuantidade.Text);
+            this.Text = "Registar Saldo";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Size = new Size(350, 250);
 
-            saldoController.AtualizarSaldo(saldo);
-            MessageBox.Show("O saldo foi alterado com sucesso!");
+            this.lblIdProduto = new Label { Text = "Id do produto:", Location = new Point(10, 40), Size = new Size(150, 20) };
+            this.txtIdProduto = new TextBox { Location = new Point(170, 40), Size = new Size(150, 20) };
+            this.lblIdAlmoxarifado = new Label { Text = "Id do almoxarifado:", Location = new Point(10, 70), Size = new Size(150, 20) };
+            this.txtIdAlmoxarifado = new TextBox { Location = new Point(170, 70), Size = new Size(150, 20) };
+            this.lblQuantidade = new Label { Text = "Quantidade:", Location = new Point(10, 100), Size = new Size(150, 20) };
+            this.txtQuantidade = new TextBox { Location = new Point(170, 100), Size = new Size(150, 20) };
+            this.btnRegister = new Button { Text = "Registrar", Location = new Point(170, 150), Size = new Size(150, 35) };
+            this.btnRegister.Click += new EventHandler(btnRegister_Click);
 
-            ListarSaldo saldoList = Application.OpenForms.OfType<ListarSaldo>().FirstOrDefault();
-            saldoList?.RefreshList();
-            Close();
+            this.Controls.Add(this.lblIdProduto);
+            this.Controls.Add(this.lblIdAlmoxarifado);
+            this.Controls.Add(this.lblQuantidade);
+            this.Controls.Add(this.txtIdProduto);
+            this.Controls.Add(this.txtIdAlmoxarifado);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.btnRegister);
+        }
+
+        private void ClearForm()
+        {
+            txtIdProduto.Clear();
+            txtIdAlmoxarifado.Clear();
+            txtQuantidade.Clear();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var saldo = new Models.Saldo(
+                    Convert.ToInt32(txtIdProduto.Text),
+                    Convert.ToInt32(txtIdAlmoxarifado.Text),
+                    Convert.ToInt32(txtQuantidade.Text)
+                );
+
+                saldoController.CriarSaldo(saldo);
+                MessageBox.Show("Saldo foi registrado com sucesso!");
+
+                ClearForm();
+
+                var saldoList = Application.OpenForms.OfType<ListarSaldo>().FirstOrDefault();
+                if (saldoList != null)
+                {
+                    saldoList.RefreshList();
+                }
+
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
